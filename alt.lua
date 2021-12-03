cd503f182f05e364fe145d029b6523287d43e80a
d <66913721+catvskid@users.noreply.github.com>
2021-12-03 14:13:08 -0300

    alt version

--- alt.lua
+++ alt.lua
@@ -1,4 +1,4 @@
--- Settings:
+-- settings:
 local NetEnabled = true -- keep this on true or this script is basically useless
--- Script Loadstring:
+-- script loadstring:
 loadstring(game:HttpGet('https://paste.ee/r/IlS9W'))() 
