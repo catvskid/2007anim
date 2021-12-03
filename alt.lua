911674bb557592d7817deb89e4a6ea541f4363e5
d <66913721+catvskid@users.noreply.github.com>
2021-12-03 14:12:49 -0300

    Update alt.lua

--- alt.lua
+++ alt.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet('https://paste.ee/r/3EfOy'))()
+-- Settings:
+local NetEnabled = true -- keep this on true or this script is basically useless
+-- Script Loadstring:
+loadstring(game:HttpGet('https://paste.ee/r/IlS9W'))() 
